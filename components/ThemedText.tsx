0a6d16e2263648216c3b4b5b30598d332389dd0c
rahulmandayal1 <rahulthakurcoder@gmail.com>
2024-10-26 01:28:53 +0530

    📝 Added text variant constants file and integrated Lato fonts with Expo

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -1,60 +1,43 @@
-import { Text, type TextProps, StyleSheet } from 'react-native';
+import { Text, type TextProps, StyleSheet } from "react-native";
+import { useThemeColor } from "@/hooks/useThemeColor";
+import {
+  useFonts,
+  Lato_400Regular,
+  Lato_400Regular_Italic,
+  Lato_700Bold,
+  Lato_700Bold_Italic,
+  Lato_900Black,
+  Lato_900Black_Italic,
+} from "@expo-google-fonts/lato";
+import { TEXT_VARIANTS } from "@/utils/textUtils";
+import { TEXT_VARIANT } from "@/constants/Text";
 
-import { useThemeColor } from '@/hooks/useThemeColor';
-
-export type ThemedTextProps = TextProps & {
+interface ThemedTextProps extends TextProps {
   lightColor?: string;
   darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
-};
+  variant?: keyof typeof TEXT_VARIANTS;
+}
 
 export function ThemedText({
   style,
   lightColor,
   darkColor,
-  type = 'default',
+  variant = TEXT_VARIANT.DEFAULT,
   ...rest
 }: ThemedTextProps) {
-  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, "text");
+  let [fontsLoaded] = useFonts({
+    [TEXT_VARIANTS.defaultText.fontFamily]: Lato_400Regular,
+    [TEXT_VARIANTS.regularItalicText.fontFamily]: Lato_400Regular_Italic,
+    [TEXT_VARIANTS.semiBoldText.fontFamily]: Lato_700Bold,
+    [TEXT_VARIANTS.semiBoldItalicText.fontFamily]: Lato_700Bold_Italic,
+    [TEXT_VARIANTS.boldText.fontFamily]: Lato_900Black,
+    [TEXT_VARIANTS.boldItalicText.fontFamily]: Lato_900Black_Italic,
+  });
 
-  return (
-    <Text
-      style={[
-        { color },
-        type === 'default' ? styles.default : undefined,
-        type === 'title' ? styles.title : undefined,
-        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
-        type === 'subtitle' ? styles.subtitle : undefined,
-        type === 'link' ? styles.link : undefined,
-        style,
-      ]}
-      {...rest}
-    />
-  );
+  if (!fontsLoaded) return null;
+
+  const combinedStyle = StyleSheet.flatten([{ color }, TEXT_VARIANTS[variant]]);
+
+  return <Text style={combinedStyle} {...rest} />;
 }
-
-const styles = StyleSheet.create({
-  default: {
-    fontSize: 16,
-    lineHeight: 24,
-  },
-  defaultSemiBold: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: '600',
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    lineHeight: 32,
-  },
-  subtitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    lineHeight: 30,
-    fontSize: 16,
-    color: '#0a7ea4',
-  },
-});
---
+++ utils/textUtils.tsx
@@ -0,0 +1,50 @@
+export const TEXT_VARIANTS = {
+  defaultText: { fontFamily: "Lato_400Regular", fontSize: 16, lineHeight: 24 },
+  regularItalicText: {
+    fontFamily: "Lato_400Regular_Italic",
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  semiBoldText: {
+    fontFamily: "Lato_700Bold",
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: "600",
+  },
+  semiBoldItalicText: {
+    fontFamily: "Lato_700Bold_Italic",
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: "600",
+  },
+  boldText: {
+    fontFamily: "Lato_900Black",
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: "900",
+  },
+  boldItalicText: {
+    fontFamily: "Lato_900Black_Italic",
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: "900",
+    titleText: {
+      fontFamily: "Lato_900Black",
+      fontSize: 32,
+      lineHeight: 32,
+      fontWeight: "bold",
+    },
+    subtitleText: {
+      fontFamily: "Lato_700Bold",
+      fontSize: 20,
+      lineHeight: 24,
+      fontWeight: "bold",
+    },
+    linkText: {
+      fontFamily: "Lato_400Regular",
+      fontSize: 16,
+      lineHeight: 30,
+      color: "#0a7ea4",
+    },
+  },
+};
